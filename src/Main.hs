79eff1a7f64c068d8e5f250561f3ef35a9095696
Nils Schweinsberg <mail@n-sch.de>
2010-06-20 15:43:22 +0200

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main () where
+
+import Prelude hiding (head, id)
+
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes hiding (title)
+import Snap.Types
+import Snap.Http.Server
+
+main = httpServe "*" 8080 "localhost"
+                 (Just "access.log")
+                 (Just "error.log")
+                 mainSnap
+
+
+mainSnap :: Snap ()
+mainSnap = writeLBS . renderHtml . html $ do
+
+    head $ do
+
+        title $ "Moep!"
+
+    body $ do
+
+        h1 ! id "moep" $ "Hi!"
+        p "Willkommen bei Snap!"
