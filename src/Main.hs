e7a558edab57600b5f64ec81071c22dabb22d43a
Nils Schweinsberg <mail@n-sch.de>
2010-06-20 16:23:29 +0200

    stylesheet, simple routing

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Text.Blaze.Html5
 import Text.Blaze.Html5.Attributes hiding (title)
 import Snap.Types
 import Snap.Http.Server
+import Snap.Util.FileServe
 
 main = httpServe "*" 8080 "localhost"
                  (Just "access.log")
@@ -16,13 +17,24 @@ main = httpServe "*" 8080 "localhost"
 
 
 mainSnap :: Snap ()
-mainSnap = writeLBS . renderHtml . html $ do
+mainSnap =
+    route [ ("/style"       , fileServeSingle' "text/css" "htdocs/style.css")
+          , ("/"            , testHtml)
+          ]
 
-    head $ do
+testHtml :: Snap ()
+testHtml = do
 
-        title $ "Moep!"
+    modifyResponse $ setContentType "text/html"
+    writeLBS . renderHtml . html $ do
 
-    body $ do
+        head $ do
 
-        h1 ! id "moep" $ "Hi!"
-        p "Willkommen bei Snap!"
+            title $ "Hello World!"
+            meta ! charset "UTF-8"
+            link ! rel "stylesheet" ! href "style" ! type_ "text/css" ! media "screen"
+
+        body $ do
+
+            h1 ! id "moep" $ "Hi!"
+            p "Hello world!"
